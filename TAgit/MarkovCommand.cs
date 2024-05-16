02d8f4089c787defd4f728f14c48b6f9fa7984b3
Nightarc <yurik.1594@gmail.com>
2024-05-16 09:17:21 +0300

    lab4

---
+++ TAgit/MarkovCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TAgit
+{
+    /// <summary>
+    /// Класс, олицетворяющий одну команду в эмуляторе НАМ
+    /// </summary>
+    internal class MarkovCommand
+    {
+        string oldStr; // искомый фрагмент команды
+        public bool stop { get; private set; }    // является ли команда остановкой
+        string newStr; // подстановка выполняемая командой
+        Regex regex;
+        public MarkovCommand(string oldStr, string newStr, bool stop)
+        {
+            this.oldStr = oldStr;
+            this.newStr = newStr;
+            this.stop = stop;
+
+            regex = new Regex(Regex.Escape(oldStr).Replace('.', '^'));
+        }
+
+        string doubleSlashes(string str)
+        {
+            if (str.Contains("/")) return str.Replace("/", "//");
+            else return str;
+        }
+        public bool Execute(string str, out string outStr) {
+            if(!regex.IsMatch(str)) { outStr = ""; return false; }
+            outStr = regex.Replace(str, newStr, 1);
+            return true;
+        }
+        public bool CanExecute(string str) => regex.IsMatch(str);
+        public override string ToString()
+        {
+            return String.Format("{0} -> {1}", oldStr, newStr);
+        }
+    }
+}
--- TAgit/Program.cs
+++ TAgit/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#pragma warning disable CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using TAgit;
+
+//  Var a : Arr_Com;
+//  alf: Set Of Char;   { входной алфавит }
+//  inp_str: String;    { входная строка }
+//  Count: Integer;     { количество команд в алгоритме}
+//  F: Boolean;         { признак корректности ввода }
+internal class Program
+{
+    static List<char> alf = new List<char>();
+    static List<MarkovCommand> commands = new List<MarkovCommand>();
+    static string input;
+    const bool DEBUG = true;
+
+    private static void InitAlphabet(string alfStr)
+    {
+        alf = alfStr.ToList();
+    }
+
+    static void Init(string fileName)
+    {
+        using (StreamReader sr = new StreamReader(fileName))
+        {
+            InitAlphabet(sr.ReadLine());
+            string inputStr = sr.ReadLine();
+            var regex = new Regex("\\d+.");
+            while (!sr.EndOfStream && regex.IsMatch(inputStr)) //what if alphabet contains /d and . as symbols?
+            {
+                inputStr = regex.Replace(inputStr, "", 1);
+                string[] commandParts = inputStr.Split('>', '!');
+                commands.Add(new MarkovCommand(commandParts[0], commandParts[1], inputStr.Contains('!')));
+
+                inputStr = sr.ReadLine();
+                if (inputStr.StartsWith("//") || inputStr == "")  inputStr = sr.ReadLine(); 
+            }
+            input = inputStr;
+        }
+    }
+
+    static string Solve() {
+        string result = input;
+        MarkovCommand executingCommand = commands[0];
+        int i = 0;
+        bool stop = false;
+        bool isCorrect = false;
+        while(!stop)
+        {
+            string temp;
+            bool executed = executingCommand.Execute(result, out temp);
+            if (executed)
+            {
+                isCorrect = true;
+                if (DEBUG) Console.WriteLine(String.Format("command {0} {1} executed {2} -> {3}", i + 1, executingCommand.ToString(), result, temp));
+                i = -1; result = temp;
+                stop = executingCommand.stop;
+            }
+            else
+            {
+                if (DEBUG) Console.WriteLine(String.Format("command {0} {1} failed", i + 1, executingCommand.ToString(), temp));
+            }
+
+            if ((i+1) % commands.Count == 0) {
+                if(!isCorrect) {
+                    Console.WriteLine("Algorithm ended because no operations were exceuted.");
+                    break;
+                }
+                isCorrect = false;
+            }
+            executingCommand = commands[(++i) % commands.Count];
+        }
+
+        return result;
+    }
+    private static void Main(string[] args)
+    {
+        Init("task2c.txt");
+        Console.WriteLine(input);
+        string result = Solve();
+        Console.WriteLine(result);
+    }
+}
+
+#pragma warning restore CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
+//task3 - слова из одной буквы не входят в область применимости
